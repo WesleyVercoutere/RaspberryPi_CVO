4ab533a5d5010fa19db5b28b53fbf44b028e6ff8
WesleyVercoutere <wesley.vercoutere@gmail.com>
2020-09-14 16:30:09 +0200

    commit

---
+++ RP-1/Sturen en lezen van de RP pinnen/GPIO_Blink_basic.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO  # https://sourceforge.net/p/raspberry-gpio-python/wiki/Home/
+import time     #https://docs.python.org/2/library/time.html?highlight=time%20time#module-time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14,GPIO.OUT)
+
+while True:
+    GPIO.output(14,GPIO.HIGH)
+    time.sleep(1)
+
+    GPIO.output(14,GPIO.LOW)
+    time.sleep(1)
